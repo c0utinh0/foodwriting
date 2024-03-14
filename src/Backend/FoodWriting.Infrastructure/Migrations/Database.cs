7c52a4c3c1c46597f05218222c21e1c97efd9eb8
c0utinh0 <douglas-couto@hotmail.com>
2024-03-14 19:15:38 -0300

    Adding User database table

---
+++ src/Backend/FoodWriting.Infrastructure/Migrations/Database.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using MySqlConnector;
+
+namespace FoodWriting.Infrastructure.Migrations;
+
+public static class Database
+{
+    public static void CreateDatabase(string connectionString, string database)
+    {
+        using var myConnection = new MySqlConnection(connectionString);
+        
+        var parameteres = new DynamicParameters();
+        parameteres.Add("name", database);
+        
+        var registers =  myConnection.Query("SELECT * FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = @name", parameteres);
+
+        if(!registers.Any())
+        {
+            myConnection.Execute($"CREATE DATABASE {database}");
+        }
+    }
+}
---
+++ src/Backend/FoodWriting.Infrastructure/Migrations/Versions/Version0001.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace FoodWriting.Infrastructure.Migrations.Versions;
+
+[Migration(1, "Create User Table")]
+public class Version0001 : Migration
+{
+    public override void Down()
+    {
+    }
+
+    public override void Up()
+    {
+        var table = Create.Table("User");
+
+        table
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("CreatedAt").AsDateTime().NotNullable()
+            .WithColumn("Name").AsString(100).NotNullable()
+            .WithColumn("Email").AsString().NotNullable()
+            .WithColumn("Password").AsString(2000).NotNullable()
+            .WithColumn("Phone").AsString(14).NotNullable();
+    }
+}
